4c9c1a7786b64279aa6b61f16a59645add347532
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-03-09 17:04:28 -0300

    exercicio03 maior de idade

---
+++ exercicios/logica-programacao/basico/exercicio03-maior-idade-menor-idade/descricao.md
@@ -0,0 +1,1 @@
+Faça um programa que receba a idade de uma pessoa e exiba se ela é maior de idade (idade igual ou superior a 18 anos) ou menor de idade.
---
+++ exercicios/logica-programacao/basico/exercicio03-maior-idade-menor-idade/script.js
@@ -0,0 +1,19 @@
+let idade = parseInt(prompt("Digite sua idade: "));
+
+if (idade >= 18) {
+  alert(`Você tem ${idade} anos. MAIOR de idade pode entrar!`);
+} else {
+  alert(`Você tem ${idade} anos. MENOR de idade, NÃO pode entrar!`);
+}
+
+/*
+// Recebe a idade do usuário
+let idade = parseInt(prompt("Digite sua idade: "));
+
+// Verifica se a idade é maior ou igual a 18
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
+*/
